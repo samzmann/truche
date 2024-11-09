9298fb5984b783a1a94adf2d880461e3f7a252f6
Samuel Mann <samuelcmann@gmail.com>
2024-11-09 18:57:24 +0100

    v2

--- index.js
+++ index.js
@@ -3,27 +3,29 @@ const gridSize = 10;
 const canvasSize = 500;
 const cellSize = canvasSize / gridSize;
 
-const colorLight = 255;
-const colorDark = 0;
+const colorBase = Math.floor(Math.random() * 360);
+const colorLight = colorBase;
+const colorDark = colorBase + 140;
 
 class Cell {
-  constructor(posX, posY, size, colorFront, colorBack) {
+  constructor(posX, posY, size, colorFront, colorBack, rotation) {
     this.posX = posX;
     this.posY = posY;
     this.size = size;
     this.colorFront = colorFront;
     this.colorBack = colorBack;
-    this.rotation = floor(random(4)) * 90;
+    this.rotation = rotation;
   }
 
   draw() {
     // Draw rectangle
-    noStroke();
-    fill(this.colorBack);
+    // noStroke();
+    stroke(255);
+    fillHsluv(this.colorBack, 90, 70);
     rect(this.posX, this.posY, this.size, this.size);
 
     // Draw shape with rotation
-    fill(this.colorFront);
+    fillHsluv(this.colorFront, 90, 70);
     push(); // Save current transformation state
     translate(this.posX + this.size / 2, this.posY + this.size / 2); // Move to center of cell
     rotate(radians(this.rotation)); // Apply rotation
@@ -46,15 +48,37 @@ function setup() {
   createCanvas(canvasSize, canvasSize);
   background(0);
 
+  let lastRotation = 0;
+  const maxRotation = 2;
   for (let i = 0; i < gridSize; i += 1) {
     for (let j = 0; j < gridSize; j += 1) {
 
-      const isDark = random(1) > 0.5;
+      const isDark = true //random(1) > 0.5;
       const randomFront = isDark ? colorDark : colorLight;
       const randomBack = isDark ? colorLight : colorDark;
-      const cell = new Cell(i * cellSize, j * cellSize, cellSize, randomFront, randomBack);
+      const rotation = lastRotation * 90;
+      lastRotation = (lastRotation + 1) % maxRotation;
+      const cell = new Cell(i * cellSize, j * cellSize, cellSize, randomFront, randomBack, rotation);
       cell.draw();
     }
   }
+
+  // Add download button
+  const downloadButton = createButton('Download Pattern');
+  downloadButton.position(10, canvasSize + 10);
+  downloadButton.mousePressed(downloadCanvas);
+}
+
+function downloadCanvas() {
+  saveCanvas('output/pattern', 'png');
+}
+
+function fillHsluv(h, s, l) {
+  var rgb = hsluv.hsluvToRgb([h, s, l]);
+  fill(rgb[0] * 255, rgb[1] * 255, rgb[2] * 255);
+}
+function strokeHsluv(h, s, l) {
+  var rgb = hsluv.hsluvToRgb([h, s, l]);
+  stroke(rgb[0] * 255, rgb[1] * 255, rgb[2] * 255);
 }
 
