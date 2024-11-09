34400a18598d541247f1a4a6a3a69741373adee9
Samuel Mann <samuelcmann@gmail.com>
2024-11-09 21:59:44 +0100

    v4

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const gridSize = 5;
+const gutterSize = 0;
 
 const canvasSize = 800;
 const cellSize = canvasSize / gridSize;
@@ -10,45 +11,112 @@ const colorDark = colorBase + 140;
 let cells; // Make cells global
 
 class Cell {
-  constructor(posX, posY, size, colorFront, colorBack, rotation) {
+  constructor(posX, posY, size, colorFront, colorBack, rotation, drawStyle) {
     this.posX = posX;
     this.posY = posY;
     this.size = size;
     this.colorFront = colorFront;
     this.colorBack = colorBack;
     this.rotation = rotation;
+    this.drawStyle = drawStyle;
+
+    this.posTopLeft = createVector(0, 0);
+    this.posTopMiddle = createVector(this.size / 2, 0);
+    this.posTopRight = createVector(this.size, 0);
+    this.posRightMiddle = createVector(this.size, this.size / 2);
+    this.posBottomRight = createVector(this.size, this.size);
+    this.posBottomMiddle = createVector(this.size / 2, this.size);
+    this.posBottomLeft = createVector(0, this.size);
+    this.posLeftMiddle = createVector(0, this.size / 2);
+
+    this.hueIncrement = 0;
   }
 
   draw() {
     // Draw rectangle
-    // noStroke();
-    stroke(255);
-    fillHsluv(this.colorBack, 90, 70);
+    noStroke();
+    fillHsluv(this.colorBack + this.hueIncrement, 90, 70);
     rect(this.posX, this.posY, this.size, this.size);
 
     // Draw shape with rotation
-    fillHsluv(this.colorFront, 90, 70);
-    push(); // Save current transformation state
-    translate(this.posX + this.size / 2, this.posY + this.size / 2); // Move to center of cell
-    rotate(radians(this.rotation)); // Apply rotation
-    translate(-this.size / 2, -this.size / 2); // Move back to origin
+    fillHsluv(this.colorFront + this.hueIncrement, 90, 70);
+    push();
+    translate(this.posX + this.size / 2, this.posY + this.size / 2);
+    rotate(radians(this.rotation));
+    translate(-this.size / 2, -this.size / 2);
 
+    if (this.drawStyle === 'lines') {
+      this.drawLines();
+    } else if (this.drawStyle === 'arcs') {
+      this.drawArcs();
+    }
+
+    pop();
+  }
+
+  drawLines() {
     beginShape();
-    vertex(0, 0);
-    vertex(this.size / 2, 0);
-    vertex(this.size, this.size / 2);
-    vertex(this.size, this.size);
-    vertex(this.size / 2, this.size);
-    vertex(0, this.size / 2);
+    vertex(this.posTopLeft.x, this.posTopLeft.y);
+    vertex(this.posTopMiddle.x, this.posTopMiddle.y);
+    vertex(this.posRightMiddle.x, this.posRightMiddle.y);
+    vertex(this.posBottomRight.x, this.posBottomRight.y);
+    vertex(this.posBottomMiddle.x, this.posBottomMiddle.y);
+    vertex(this.posLeftMiddle.x, this.posLeftMiddle.y);
     endShape(CLOSE);
+  }
 
-    pop(); // Restore transformation state
+  drawArcs() {
+    // Draw straight lines for top-left segment
+    // beginShape();
+    // vertex(this.posTopLeft.x, this.posTopLeft.y);
+    // vertex(this.posTopMiddle.x, this.posTopMiddle.y);
+
+    // Draw arc from top-middle to right-middle (bending inward)
+
+    // arc(this.posTopRight.x, this.posTopRight.y,  // center x,y
+    //   this.size, this.size,    // width, height
+    //   PI, -HALF_PI,           // start angle, end angle
+    //   OPEN);                  // don't close the arc
+
+    arc(this.size, 0, this.size, this.size, PI / 2, PI);
+
+
+
+    // Draw straight lines for right-bottom segment
+
+    // vertex(this.posRightMiddle.x, this.posRightMiddle.y);
+    // vertex(this.posBottomRight.x, this.posBottomRight.y);
+    // vertex(this.posBottomMiddle.x, this.posBottomMiddle.y);
+
+
+    // Draw arc from bottom-middle to left-middle (bending inward)
+
+    // arc(0, this.size / 2,         // center x,y
+    //   this.size, this.size,    // width, height
+    //   HALF_PI, 0,             // start angle, end angle
+    //   OPEN);                  // don't close the arc
+
+    arc(0, this.size, this.size, this.size, -PI / 2, 0);
+
+
+    // Draw straight line for left-top segment
+
+    // vertex(this.posLeftMiddle.x, this.posLeftMiddle.y);
+    // vertex(this.posTopLeft.x, this.posTopLeft.y);
+    // endShape(CLOSE);
+  }
+
+  update() {
+    this.hueIncrement += 0.5;
+    // Update the colors based on the increment
+    // this.colorFront = (this.colorFront + this.hueIncrement) % 360;
+    // this.colorBack = (this.colorBack + this.hueIncrement) % 360;
   }
 }
 
 function setup() {
   createCanvas(canvasSize, canvasSize);
-  background(0);
+  background(255);
 
   // Update cells initialization to use global variable
   cells = Array(gridSize).fill().map(() => Array(gridSize));
@@ -59,14 +127,16 @@ function setup() {
       const isDark = random(1) > 0.5;
       const randomFront = isDark ? colorDark : colorLight;
       const randomBack = isDark ? colorLight : colorDark;
+      const drawStyle = 'arcs';
 
       cells[i][j] = new Cell(
-        i * cellSize,
-        j * cellSize,
-        cellSize,
+        i * cellSize + gutterSize,
+        j * cellSize + gutterSize,
+        cellSize - gutterSize * 2,
         randomFront,
         randomBack,
-        0 // temporary rotation
+        0, // temporary rotation
+        drawStyle
       );
     }
   }
@@ -87,6 +157,21 @@ function setup() {
   downloadButton.position(10, canvasSize + 10);
   downloadButton.mousePressed(downloadCanvas);
 
+}
+
+function draw() {
+  background(255);
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      cells[i][j].update();
+    }
+  }
+
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      cells[i][j].draw();
+    }
+  }
 }
 
 function downloadCanvas() {
